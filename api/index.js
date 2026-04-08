91e1391c688f5e2fdccf5985975925ee3eec8a0f
Medcherif01 <cherifmed2010@gmail.com>
2026-04-08 07:26:45 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -127,7 +127,7 @@ if (VAPID_PUBLIC_KEY && VAPID_PRIVATE_KEY) {
 }
 
 const arabicTeachers = ['Sara', 'Amal Najar', 'Emen', 'Fatima', 'Ghadah', 'Hana'];
-const englishTeachers = ['Jana','Amal','Farah','Tayba','Shanoja'];
+const englishTeachers = ['Jana','Amal','Farah','Tayba','Nana'];
 
 const specificWeekDateRangesNode = {
   1:{start:'2025-08-31',end:'2025-09-04'}, 2:{start:'2025-09-07',end:'2025-09-11'}, 3:{start:'2025-09-14',end:'2025-09-18'}, 4:{start:'2025-09-21',end:'2025-09-25'}, 5:{start:'2025-09-28',end:'2025-10-02'}, 6:{start:'2025-10-05',end:'2025-10-09'}, 7:{start:'2025-10-12',end:'2025-10-16'}, 8:{start:'2025-10-19',end:'2025-10-23'}, 9:{start:'2025-10-26',end:'2025-10-30'},10:{start:'2025-11-02',end:'2025-11-06'},
@@ -140,7 +140,7 @@ const validUsers = {
   "Mohamed": "Mohamed", "Zohra": "Zohra", "Jana": "Jana", "Aichetou": "Aichetou",
   "Amal": "Amal", "Amal Najar": "Amal Najar", "Ange": "Ange", "Anouar": "Anouar",
   "Emen": "Emen", "Farah": "Farah", "Fatima": "Fatima", "Ghadah": "Ghadah",
-  "Hana": "Hana", "Samira": "Samira", "Tayba": "Tayba", "Shanoja": "Shanoja",
+  "Hana": "Hana", "Samira": "Samira", "Tayba": "Tayba", "Nana": "Nana",
   "Sara": "Sara", "Souha": "Souha", "Inas": "Inas"
 };
 
